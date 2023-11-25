22f4cbe9c2f9b9ccf0973374c3f7a9f10201726e
Saeed0920 <sd.eed1381@gmail.com>
2023-11-25 12:46:28 +0330

    feat: Add header-section

---
+++ src/components/header.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { navbar } from "@/components/ui";
+</script>
+<template>
+  <main class="flex flex-col">
+    <navbar />
+    <!-- TODO: Set Img for background -->
+    <section class="w-full h-[85vh] grid grid-cols-1 grid-rows-1">
+      <img
+        src="#"
+        alt="Main-background"
+        class="h-full w-full col-start-1 row-start-1"
+      />
+      <div
+        class="self-center justify-self-start ml-[25vw] flex flex-col items-center col-start-1 row-start-1"
+      >
+        <h1>lorem header</h1>
+        <p>Im Saeed :)))</p>
+      </div>
+    </section>
+  </main>
+</template>
+<style scoped></style>
---
+++ src/components/ui/navbar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+<template>
+  <nav
+    class="grid items-center grid-cols-[repeat(5,4rem)] justify-center gap-2"
+  >
+    <!-- TODO: insert Logo Template for Logo -->
+    <img src="#" alt="Logo" class="w-4 h-4" />
+    <!-- TODO: fix href for each link -->
+    <a href="#">Header</a>
+    <a href="#">Main</a>
+    <a href="#">Gallery</a>
+    <a href="#">Footer</a>
+  </nav>
+</template>
+
+<style scoped></style>
