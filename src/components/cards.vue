e7c796f8a1592a068d62a4c9b3ee8d561a24d57e
Saeed0920 <sd.eed1381@gmail.com>
2023-11-25 12:47:01 +0330

    feat: add mainComponent as cards

---
+++ src/components/cards.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+<template>
+  <main class="grid">main section for cards and items</main>
+</template>
+
+<style scoped>
+main {
+  grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+}
+</style>
